a8f5cef6711f0c1b4b22e7fb741dd9c271945af6
Tera Kirk <terakrk@gmail.com>
2023-09-08 14:37:51 -0500

    got operate function to work

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 'use strict';
 
-let num1;
-let num2;
-let operator;
+let firstNumber = 0;
+let secondNumber = 0;
+const operators = ['+', '-', '*', '/'];
 
-function add(num1, num2) {
-    let sum = num1 + num2;
-    return sum;
+
+const add = function(num1, num2) {
+    let result = num1 + num2;
+    return result;
 }
 
-function subtract(num1, num2) {
-   if (num2 > num1) {
-    let result = num2 - num1;
-    return result;
-   } else {
-    let result = num1 - num2;
-    return result;
-   }
+const subtract = function(num1, num2) {
+    if (num2 > num1) {
+        let result = num2 - num1;
+        return result;
+    } else {
+        let result = num1 - num2;
+        return result;
+    }
 }
 
-function multiply(num1, num2) {
+const multiply = function(num1, num2) {
     let result = num1 * num2;
     return result;
 }
 
-function divide(num1, num2) {
+const divide = function(num1, num2) {
     if (num2 > num1) {
         let result =  num2 / num1;
         return result;
@@ -33,3 +34,17 @@ function divide(num1, num2) {
         return result;
     }
 }
+
+const operate = (num1, operator, num2) => {
+   if (operator === '+') {
+    return add(num1, num2);
+   } else if (operator === '-') {
+    return subtract(num1, num2);
+   } else if (operator === '*') {
+    return multiply(num1, num2); 
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    } else {
+        return 'Invalid Operation';
+    }
+   };
