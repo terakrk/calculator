215cedd744cd4a8e675f37b2c2642e4e7f69bad0
Tera Kirk <terakrk@gmail.com>
2023-09-09 14:38:46 -0500

    inputs to display and clears display

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 'use strict';
 
-let firstNumber = 0;
-let secondNumber = 0;
-const operators = ['+', '-', '*', '/'];
 const display = document.querySelector('#display');
+
+const firstNumber = document.createElement('span');
+firstNumber.classList.add('first-number');
+display.appendChild(firstNumber);
+
+const secondNumber = document.createElement('span');
+    secondNumber.classList.add('second-number');
+    display.appendChild(secondNumber);
+
+const arithmeticOperator = document.createElement('span');
+    arithmeticOperator.classList.add('operator');
+    display.appendChild(arithmeticOperator);
+    
 const numbers = document.querySelectorAll('.number');
-numbers.forEach(number => {
+    numbers.forEach(number => {
     number.addEventListener('click', e => {
-        console.log(e.target.id);
+        if (!arithmeticOperator) {
+            firstNumber.textContent = e.target.id;
+        } else {
+            secondNumber.textContent = e.target.id;
+        }
     })
 })
 
+const operators = document.querySelectorAll('.operator');
+    operators.forEach(operator => {
+        operator.addEventListener('click', e => {
+           arithmeticOperator.textContent = e.target.id;
+        })
+    })
 
+const clear = document.querySelector('#clear');
 const add = function(num1, num2) {
     let result = num1 + num2;
     return result;
@@ -55,3 +76,15 @@ const operate = (num1, operator, num2) => {
         return 'Invalid Operation';
     }
    };
+
+   const clearDisplay = function() {
+        const junk = document.querySelectorAll('span');
+        const junkArray= [...junk];
+        junkArray.forEach(span => {
+            span.remove();
+        });
+    };
+   const equals = document.querySelector('.equals');
+   equals.addEventListener('click', operate);
+
+   clear.addEventListener('click', clearDisplay);
