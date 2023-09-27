4e5c2ba5ca063d3796bb6f6b8dbec270dd733cf7
Tera Kirk <terakrk@gmail.com>
2023-09-27 17:50:18 -0500

    decimal point works now mostly

--- script.js
+++ script.js
@@ -1,55 +1,52 @@
 'use strict';
-let firstNumber = '';
-let secondNumber = '';
+let firstNumber = Number();
+let secondNumber = Number();
 let mathOperator = '';
 let answer = Number();
 
 
 
 
+
 const numberDisplay = document.querySelector('#number-display');
-
-//const firstNumber = document.querySelector('#first-number');
-
-//const secondNumber = document.querySelector('#second-number');
-
-//const arithmeticOperator = document.querySelector('#arithmetic-operator');
     
 const numbers = Array.from(document.querySelectorAll('.number'));
     numbers.forEach(number => {
     number.addEventListener('click', e => {
         numberDisplay.textContent += e.target.id;
     if (!mathOperator){
-        firstNumber += e.target.id;
+        firstNumber += parseFloat(e.target.id);
     } else {
-        secondNumber += e.target.id;
         document.getElementById('.').disabled = false;
+        secondNumber += parseFloat(e.target.id);
+       
     }
-        if(e.target.id ==='.') {
+   if(e.target.id ==='.') {
     document.getElementById('.').disabled = true;
+   }
+   
 });
     });
+    
 const operators = Array.from(document.querySelectorAll('.operator'));
     operators.forEach(operator => {
        operator.addEventListener('click', e => {
             numberDisplay.textContent += e.target.id;
             mathOperator = e.target.id;
-            mathChain();
         })
+        mathChain();
     })
 
 const clear = document.querySelector('#clear');
 const add = function(num1, num2) {
-    num1 = parseFloat(firstNumber);
-    num2 = parseFloat(secondNumber);
+    mathChain();
    numberDisplay.textContent = num1 + num2;
    answer = num1 + num2;
    return answer;
-}
+};
 
 const subtract = function(num1, num2) {
-    num1 = parseFloat(firstNumber);
-    num2 = parseFloat(secondNumber);
+    mathChain();
     if (num2 > num1) {
         numberDisplay.textcontent = num2 - num1;
         answer = num2 - num1;
@@ -61,16 +58,13 @@ const subtract = function(num1, num2) {
 }
 
 const multiply = function(num1, num2) {
-    num1 = parseFloat(firstNumber);
-    num2 = parseFloat(secondNumber);
     numberDisplay.textContent = num1 * num2;
     answer = num1 * num2;
     return answer;
 }
 
 const divide = function(num1, num2) {
-    num1 = parseFloat(firstNumber);
-    num2 = parseFloat(secondNumber);
+    mathChain();
     if (num2 > num1) {
         numberDisplay.textContent = num2 / num1;
         answer = num2 / num1;
@@ -83,6 +77,7 @@ const divide = function(num1, num2) {
 
 const operate = (operator, num1, num2) => {
     operator = mathOperator;
+    mathChain();
    if (mathOperator === '+') {
     return add(num1, num2);
    } else if (operator === '-') {
@@ -99,10 +94,11 @@ const operate = (operator, num1, num2) => {
 const clearDisplay = function() {
     const junk = document.querySelector('#number-display')
     junk.textContent= '';
-     firstNumber = '';
-     secondNumber = '';
+     firstNumber = Number();
+     secondNumber = Number();
      mathOperator = '';
-     answer = '';
+     answer = Number();
+     document.getElementById('.').disabled = false;
     };
 
    const equals = document.querySelector('.equals');
@@ -111,10 +107,13 @@ const clearDisplay = function() {
    });
    clear.addEventListener('click', clearDisplay);
 
-   function mathChain() {
+function mathChain(num2, num1) {
+    num2 = parseFloat(secondNumber);
     if(!answer) {
-        return firstNumber;
+        num1 = parseFloat(firstNumber);
     } else {
-        return answer = firstNumber;
+        num1 = parseFloat(answer);
     }
-   }
+}
+    
+   
